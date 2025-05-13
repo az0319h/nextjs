e56a7d9b7cb99252ceafd12b1f4551adaf1e5807
LEE JI SU <afafmm1903@gmail.com>
2025-05-13 16:39:12 +0900

    초기셋팅완료

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>My App Page</h1>
+      <p>Welcome!</p>
+    </main>
+  );
+}
