fd845cfafffa4ec26f343d6fe177a2dbd49988e1
az0319h <az0319h@naver.com>
2025-05-14 19:19:02 +0900

    글로벌 CSS, Layout 수정

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      {/* <div className="grid gap-3 grid-cols-2 md:grid-cols-3 [&>div]:text-center [&>div]:bg-amber-500">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        // UI테스트용 코드
+        //  주석은 모두 지워주세요!
+      </div> */}
+      HomePage
+    </>
+  )
 }
