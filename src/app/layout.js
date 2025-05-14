eab815aa0bad812b0d8bccb004f03eb0dfa7e3bf
root <root@DESKTOP-AFSEKMO.>
2025-05-14 18:09:54 +0900

    Style: root layout 너비 수정 + Header 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import "./../components/styles/index.css";
+import Header from "@/components/layout/Header";
 
 export const metadata = {
   title: "최애의 포토",
@@ -10,7 +11,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>
-        <main className="mx-auto min-h-screen w-full sm:max-w-[375px] md:max-w-[744px] lg:max-w-[1920px]">
+        <Header />
+        <main className="mx-auto min-h-screen w-full sm:max-w-[345px] md:max-w-[704px] lg:max-w-[1480px]">
           {children}
         </main>
       </body>
