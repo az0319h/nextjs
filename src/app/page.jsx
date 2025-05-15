13c8f7de4b253bcc87ca6b4d26c249b37824730f
az0319h <az0319h@naver.com>
2025-05-15 16:53:05 +0900

    dev-stop

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,6 +10,7 @@ export default function HomePage() {
         <div>Input컴포넌트 확인용 페이지 클릭</div>
       </Link>
       배포 확인(배포에 문제가 있는 확인)
+      제발DEV야 좀 .....
       <RandomPoint />
     </div>
   );
