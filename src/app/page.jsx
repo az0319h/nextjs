45ae86e89d32c9c47852e5875dbaa2be3a9fafee
fs6-kde <kde040508@gmail.com>
2025-05-15 10:11:39 +0900

    feat: input 기능

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main></main>;
+import Link from "next/link";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      HomePage
+      <Link href="/signup">
+        <div>Input컴포넌트 확인용 페이지 클릭</div>
+      </Link>
+    </div>
+  );
 }
---
+++ src/components/ui/InputField.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function InputField({
+  label,
+  placeholder,
+  type = "text",
+  value,
+  onChange,
+  name,
+  labelClassName = "",
+  error = "",
+}) {
+  const isError = Boolean(error);
+
+  return (
+    <div className="w-full flex flex-col gap-2">
+      {label && (
+        <label className={`text-white ${labelClassName}`}>{label}</label>
+      )}
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={`w-full px-4 py-3 border border-gray-400 rounded bg-black text-white placeholder-gray-400 placeholder:text-sm focus:outline-none focus:ring-2 focus:ring-white
+          ${
+            isError
+              ? "border border-red-500 focus:ring-red-500"
+              : "border border-gray-400 focus:ring-white"
+          }`}
+      />
+      {isError && <p className="text-red-500 text-sm">{error}</p>}
+    </div>
+  );
+}
+
+export default InputField;
