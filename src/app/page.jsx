959792ace54dedae8f4b7bd47704e0d78c8e669f
Wooju <dnwn810@naver.com>
2025-05-15 13:21:38 +0900

    fix: 빌드 실행시 오류 제거

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,7 @@
+"use client";
 import CardListPageEx from "@/components/FllterDropdown/CardListPageEx";
+import { Suspense } from "react"; //useSearchParams를 사용해서 Suspense로 감싸야 안전하게 CSR 처리가 됩니다.
+
 import Link from "next/link";
 import React from "react";
 
@@ -6,7 +9,9 @@ export default function HomePage() {
   return (
     <div>
       HomePage
-      <CardListPageEx />
+      <Suspense fallback={null}>
+        <CardListPageEx />
+      </Suspense>
       <Link href="/signup">
         <div>Input컴포넌트 확인용 페이지 클릭</div>
       </Link>
